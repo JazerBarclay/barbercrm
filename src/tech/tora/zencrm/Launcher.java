9f0ea38cf845945c99da91874f688b9e3922cef6
Jazer Barclay <contact@jazer.co.uk>
2020-02-06 15:06:24 +0000

    Initial Setup

---
+++ src/tech/tora/zencrm/Launcher.java
@@ -0,0 +1,13 @@
+package tech.tora.zencrm;
+
+public class Launcher {
+
+	public Launcher() {
+		System.out.println("Hello World! Zen CRM here");
+	}
+	
+	public static void main(String[] args) {
+		new Launcher();
+	}
+
+}
