9d5dc5ee395b8ab8076aa82b0cc228246aa64822
jazer <jazer@tora.tech>
2020-02-09 15:58:41 +0000

    Created customertypes

---
+++ src/tech/tora/barbercrm/widgets/CustomerType.java
@@ -0,0 +1,28 @@
+package tech.tora.barbercrm.widgets;
+
+/* PLEASE NOTE : DATABASE POPULATES VALUES BASED ON THESE ENUMS */
+/* ADD NEW VALUES IN SEQUENTIAL ORDER TO PRESERVE DB ID VALUES  */
+
+/**
+ * Type of customer
+ */
+public enum CustomerType {
+	
+	CHILD(1),
+	ADULT(2),
+	OAP(3)
+	;
+	
+	/**
+	 * Database ID
+	 */
+	public int id;
+	
+	/**
+	 * Customer Type Name and ID
+	 * @param id - Database ID Reference
+	 */
+	CustomerType(int value) {
+		this.id = value;
+	}
+}
