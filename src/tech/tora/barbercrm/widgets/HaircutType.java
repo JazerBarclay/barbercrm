a5531d22823f6ccf75d42c9c30baee7ef70550e7
jazer <jazer@tora.tech>
2020-02-09 15:59:08 +0000

    Added haircuttypes

--- src/tech/tora/barbercrm/widgets/HaircutType.java
+++ src/tech/tora/barbercrm/widgets/HaircutType.java
@@ -1,5 +1,11 @@
-package tech.tora.zencrm;
+package tech.tora.barbercrm.widgets;
 
+/* PLEASE NOTE : DATABASE POPULATES VALUES BASED ON THESE ENUMS */
+/* ADD NEW VALUES IN SEQUENTIAL ORDER TO PRESERVE DB ID VALUES  */
+
+/**
+ * Type of haircut a customer can receive
+ */
 public enum HaircutType {
 
 	OAP(1),
@@ -16,10 +22,16 @@ public enum HaircutType {
 	HIGHLIGHTSCUT(12)
 	;
 	
+	/**
+	 * Database ID
+	 */
 	public int id;
 	
+	/**
+	 * Haircut Type Name and ID
+	 * @param id - Database ID Reference
+	 */
 	HaircutType(int id) {
 		this.id = id;
 	}
-	
 }
