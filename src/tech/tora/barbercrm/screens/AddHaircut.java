55c9f0254ad778bfd2ac5582b2cf95dfcb127d80
Jazer Barclay <contact@jazer.co.uk>
2020-02-12 10:51:08 +0000

    Added frame

--- src/tech/tora/barbercrm/screens/AddHaircut.java
+++ src/tech/tora/barbercrm/screens/AddHaircut.java
@@ -1,5 +1,48 @@
 package tech.tora.barbercrm.screens;
 
-public class AddHaircut {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import tech.tora.tools.swing.frame.AdvancedFrame;
+
+public class AddHaircut extends AdvancedFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JFrame parentFrame = null;
+	
+	public AddHaircut(JFrame parent) {
+		this.parentFrame = parent;
+		
+		setSize(400, 600);
+		setTitle("Add Haircut");
+		setContentPane(new JPanel());
+		setLocationRelativeTo(null);
+	}
+	
+	@Override
+	public void openAction() {}
+
+	@Override
+	public void closeAction() {
+		this.dispose();
+		parentFrame.enable(true);
+		parentFrame.requestFocus();
+	}
+
+	@Override
+	public void minimiseAction() {}
+
+	@Override
+	public void maximiseAction() {}
+
+	@Override
+	public void gainFocusAction() {}
+
+	@Override
+	public void loseFocusAction() {}
 
 }
