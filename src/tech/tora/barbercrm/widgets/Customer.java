556ef42f2a4179a4e45db4fca417d2af3c8d4d95
jazer <jazer@tora.tech>
2020-03-01 13:40:57 +0000

    Removed spacing

--- src/tech/tora/barbercrm/widgets/Customer.java
+++ src/tech/tora/barbercrm/widgets/Customer.java
@@ -3,19 +3,12 @@ package tech.tora.barbercrm.widgets;
 public class Customer {
 	
 	private int customerID;
-
 	private String firstName, lastName;
-
 	private CustomerType customerType;
-	
 	private int currentStreak;
-	
 	private int totalFreeHaircuts;
-	
 	private String email;
-	
 	private String comments;
-	
 	private boolean active;
 
 	public Customer() {
